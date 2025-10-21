05066f45db11c7dcd35ea21b9445af79419d80b0
mtcalvop01 <mtcalvop01@iesalbarregas.es>
2025-10-22 00:07:28 +0200

    Tarea de Prototipado, añadiendo css y html como el de figma (versión1)

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('menu-toggle');
+const nav = document.querySelector('nav');
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
